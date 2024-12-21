0fb5082a60183f1df0225867e2d52111a32c74c8
roodletoof <68161791+roodletoof@users.noreply.github.com>
2024-12-21 12:57:40 +0100

    added realloc

--- malloc_group.h
+++ malloc_group.h
@@ -2,7 +2,7 @@
  * License: MIT
  *
  * A tiny header-only library for scoped heap allocation by treating each
- * allocated chunk of memory as a node in a singly linked list.
+ * allocated chunk of memory as a node in a linked list.
  *
  * Example:
  * 
@@ -29,6 +29,7 @@
 
 	typedef struct malloc_node {
 		struct malloc_node *next;
+		struct malloc_node *prev;
 		size_t data_size;
 		unsigned char data[];
 	} malloc_node_t;
@@ -40,6 +41,7 @@
 	#define MG_SCOPED &((malloc_group_t){NULL})
 
 	void *mg_malloc(malloc_group_t *mg, size_t size);
+	void *mg_realloc(malloc_group_t *mg, void *ptr, size_t size);
 	void mg_free(malloc_group_t *mg);
 
 
@@ -47,15 +49,24 @@
 
 		void *mg_malloc(malloc_group_t *mg, size_t size) {
 			malloc_node_t *node = malloc(sizeof(malloc_node_t) + size);
-
 			assert(node != NULL);
-
 			node->data_size = size;
 			node->next = mg->head;
-
+			node->prev = NULL;
+			node->next->prev = node;
 			mg->head = node;
-
 			return &node->data;
+		}
+
+		void *mg_realloc(malloc_group_t *mg, void *ptr, size_t size) {
+			malloc_node_t *old_node = ((malloc_node_t*) ptr)-1;
+			malloc_node_t new_node = realloc(old_node, sizeof(malloc_node_t) + size);
+			assert(new_node != NULL);
+			if (new_node->prev != NULL) new_node->prev->next = new_node;
+			if (new_node->next != NULL) new_node->next->prev = new_node;
+			if (mg->head == old_node) mg->head = new_node;
+			new_node->size = size;
+			return &new_node->data;
 		}
 
 		void mg_free(malloc_group_t *mg) {
